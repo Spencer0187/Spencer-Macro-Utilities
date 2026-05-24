8b452a5186e60a2b154f6c1073356c65b05b2207
Floofy <53851147+FloofyIV@users.noreply.github.com>
2026-05-24 12:33:04 +1200

    Update launcher.sh

--- AppImage/launcher.sh
+++ AppImage/launcher.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 NETHELPER="/usr/bin/nethelper"
 TMP="/tmp/nethelper-$USER"
 
-if ! pgrep -x "nethelper-$USER" >/dev/null 2>&1; then
+if ! pgrep -f "$TMP" >/dev/null 2>&1; then
   rm -f "$TMP"
   cp "$NETHELPER" "$TMP"
   chmod +x "$TMP"
